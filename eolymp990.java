6dc3d259f5c52893fe5c7934b60d58fc35ec4810
etibarliparvin <73303441+etibarliparvin@users.noreply.github.com>
2020-11-12 14:47:37 +0400

    Add files via upload

---
+++ eolymp990.java
@@ -0,0 +1,7 @@
+package EolympFromOneToTousand;
+
+public class eolymp990 {
+    public static void main(String[] args) {
+        System.out.println("1" + "\n" + "2" + "\n" + "3" + "\n" + "4" + "\n" + "5");
+    }
+}
