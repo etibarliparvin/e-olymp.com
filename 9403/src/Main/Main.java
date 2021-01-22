a31c3deb31989ad09d9bd35a56c473c15ceedf19
Etibarli Parvin <etibarliparvin@gmail.com>
2021-01-22 15:26:36 +0400

    9403

---
+++ 9403/src/Main/Main.java
@@ -0,0 +1,22 @@
+package Main;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        n = n * 2 - 1;
+        for(int a = 0; a < n; a++){
+            for(int b = 0; b < n; b++){
+                if(a == b || a + b == n - 1){
+                    System.out.print("*");
+                }else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
