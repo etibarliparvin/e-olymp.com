f378858e7879fc3cc400c16d6faf5e8daed528ac
Etibarli Parvin <etibarliparvin@gmail.com>
2021-01-23 12:28:40 +0400

    8944

---
+++ 8944/src/Main/Main.java
@@ -0,0 +1,21 @@
+package Main;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        for(int a = 0; a < n; a++){
+            for(int b = 0; b < n; b++){
+                if(a == 0 || a == n - 1 || b == 0 || b == n - 1){
+                    System.out.print("*");
+                }else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
