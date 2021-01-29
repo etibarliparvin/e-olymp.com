e081a90aa5df087dd56d74fbb4945650fac99b82
Etibarli Parvin <etibarliparvin@gmail.com>
2021-01-29 18:39:38 +0400

    e-olymp

--- 62/src/Main/Main.java
+++ 62/src/Main/Main.java
@@ -7,6 +7,15 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        
+        BigInteger big = in.nextBigInteger();
+        BigInteger result = new BigInteger("1");
+        for(int i = 2; i < 2002; i++){
+            if(result.equals(big)){
+                System.out.println(i - 1);
+                break;
+            }
+            BigInteger temp = new BigInteger(String.valueOf(i));
+            result = result.multiply(temp);
+        }
     }
 }
